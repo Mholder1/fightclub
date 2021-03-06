22a02b4fa64f8e12b261b3d72254a6f72353bc07
Mholder1 <marcus.holder@gmail.com>
2021-03-06 17:00:14 +0000

    fightclub api

--- fightclub.py
+++ fightclub.py
@@ -1,5 +1,10 @@
 import json
 
+def read_table():
+    with open('fightstats.json', 'r') as f:
+        return json.loads(f.read())
+
+
 def input_fightstats():    
     name = input("Enter your name: ")
     matchup = input("Who was your opponent?: ")
@@ -8,40 +13,39 @@ def input_fightstats():
     matchup = matchup.capitalize()
     winner = winner.capitalize()
 
-    with open('fightstats.json', 'r') as f:
-        HousematesL = json.loads(f.read())
+    HousematesL = read_table()
 
-        draw = False
-        if name == winner:
-            loser = matchup
-            winner = name
-        elif matchup == winner:
-            winner = matchup
-            loser = name
-        else:
-            draw = True
+    draw = False
+    if name == winner:
+        loser = matchup
+        winner = name
+    elif matchup == winner:
+        winner = matchup
+        loser = name
+    else:
+        draw = True
 
-        for person in range(len(HousematesL['contestants'])):  
-            if winner in HousematesL['contestants'][person]['name']:
-                HousematesL['contestants'][person]['wins'] = HousematesL['contestants'][person]['wins'] + 1
+    for person in range(len(HousematesL['contestants'])):  
+        if winner in HousematesL['contestants'][person]['name']:
+            HousematesL['contestants'][person]['wins'] = HousematesL['contestants'][person]['wins'] + 1
 
-            if loser in HousematesL['contestants'][person]['name']:
-                HousematesL['contestants'][person]['losses'] = HousematesL['contestants'][person]['losses'] + 1
+        if loser in HousematesL['contestants'][person]['name']:
+            HousematesL['contestants'][person]['losses'] = HousematesL['contestants'][person]['losses'] + 1
 
-            if draw:
-                if name in HousematesL['contestants'][person]['name']:
-                    HousematesL['contestants'][person]['draws'] = HousematesL['contestants'][person]['draws'] + 1
-                if matchup in HousematesL['contestants'][person]['name']:
-                    HousematesL['contestants'][person]['draws'] = HousematesL['contestants'][person]['draws'] + 1
+        if draw:
+            if name in HousematesL['contestants'][person]['name']:
+                HousematesL['contestants'][person]['draws'] = HousematesL['contestants'][person]['draws'] + 1
+            if matchup in HousematesL['contestants'][person]['name']:
+                HousematesL['contestants'][person]['draws'] = HousematesL['contestants'][person]['draws'] + 1
 
-        for value in HousematesL:
-            sorted_data = sorted(HousematesL['contestants'], key = lambda numbers: numbers['wins'], reverse=True)
-            fightfile = json.dumps({ 'contestants': sorted_data}, indent = 2)
+    for value in HousematesL:
+        sorted_data = sorted(HousematesL['contestants'], key = lambda numbers: numbers['wins'], reverse=True)
+        fightfile = json.dumps({ 'contestants': sorted_data}, indent = 2)
 
 
-        with open('fightstats.json', 'w') as f:
-            f.write(fightfile)
-            print(fightfile)
+    with open('fightstats.json', 'w') as f:
+        f.write(fightfile)
+        print(fightfile)
     
 
 
---
+++ fightclub_api.py
@@ -0,0 +1,36 @@
+import json
+from flask import Flask, request, render_template
+from flask_restful import Api
+from flask import Response
+import fightclub
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+def post_params_okay(mandatory_fields, req_data):
+    for field in mandatory_fields:
+        if field in req_data:
+            continue
+        else:
+            return False
+    return True
+
+@app.route('/addfight', methods=['POST'])
+def addfight():
+    fight_data = request.get_json(force=True)
+    if fight_data is None:
+        return Response("No fight data posted.", 400, mimetype="text/plain")
+    if not post_params_okay(("name", "winner", "matchup"), fight_data):
+        return Response("Missing field data, must supply name, winner and matchup", 400, mimetype="text/plain")
+
+@app.route('/gettable')
+def gettable():
+    table = fightclub.read_table()
+    return Response(json.dumps(table), 200, mimetype="application/json")
+
+if __name__ == '__main__':
+  app.run(debug=True)
