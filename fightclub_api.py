69c7fea706858dcd7dae60c9ec9aaa5551b5adb4
Mholder1 <marcus.holder@gmail.com>
2021-04-01 17:39:55 +0100

    testing

--- fightclub_api.py
+++ fightclub_api.py
@@ -21,11 +21,13 @@ def post_params_okay(mandatory_fields, req_data):
 
 @app.route('/addfight', methods =['POST'])
 def addfight():
-    #fight_data = request.get_json(force=True)
-    #if fight_data is None:
-        #return Response("No fight data posted.", 400, mimetype="text/plain")
-    #if not post_params_okay(("name", "winner", "matchup"), fight_data):
-        #return Response("Missing field data, must supply name, winner and matchup", 400, mimetype="text/plain")
+    fight_data = request.get_json(force=True)
+    sort_fight = fightclub.input_fightstats()
+
+    if fight_data is None:
+        return Response("No fight data posted.", 400, mimetype="text/plain")
+    if not post_params_okay(("name", "winner", "matchup"), fight_data):
+        return Response("Missing field data, must supply name, winner and matchup", 400, mimetype="text/plain")
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
