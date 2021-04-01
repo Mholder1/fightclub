d5c0e83487af0823e9f9e3d1327f956b2ffaca24
Mholder1 <marcus.holder@gmail.com>
2021-04-01 17:28:20 +0100

    adding amend_table

--- fightclub_api.py
+++ fightclub_api.py
@@ -21,11 +21,12 @@ def post_params_okay(mandatory_fields, req_data):
 
 @app.route('/addfight', methods =['POST'])
 def addfight():
-    #fight_data = request.get_json(force=True)
-    #if fight_data is None:
-        #return Response("No fight data posted.", 400, mimetype="text/plain")
-    #if not post_params_okay(("name", "winner", "matchup"), fight_data):
-        #return Response("Missing field data, must supply name, winner and matchup", 400, mimetype="text/plain")
+    fight_data = request.get_json(force=True)
+    sorted_fights = fightclub.amend_table(name, matchup, winner)
+    if fight_data is None:
+        return Response("No fight data posted.", 400, mimetype="text/plain")
+    if not post_params_okay(("name", "winner", "matchup"), fight_data):
+        return Response("Missing field data, must supply name, winner and matchup", 400, mimetype="text/plain")
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 
