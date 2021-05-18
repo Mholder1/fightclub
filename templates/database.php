c20c4dde6da08cdfeac14e81b809fabaccc19651
Mholder1 <marcus.holder@gmail.com>
2021-05-18 14:44:10 +0100

    fightpush

---
+++ templates/database.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$dbServername = "localhost";
+$dbPort = "5432"
+$dbUsername = "postgres";
+$dbName = "postgres";
+$dbPassword = "root";
+
+$connect = mysql_connect($dbServername, $dbPort, $dbUsername, $dbName, $dbPassword);
+
+?>
