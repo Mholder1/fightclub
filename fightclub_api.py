8ab35ad78e358aacaef6e7d817a4fccb5e70f615
Mholder1 <marcus.holder@gmail.com>
2021-05-26 17:11:01 +0100

    attempting to display users

--- fightclub_api.py
+++ fightclub_api.py
@@ -17,41 +17,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 POSTGRES_URL = "postgres://vtxojwza:iYRO1OEyap8wRAhe_xrGA_-w2Pgvv10p@rogue.db.elephantsql.com/vtxojwza"
 
 
-connection = psycopg2.connect(POSTGRES_URL)
-try:
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "CREATE TABLE fight_table (name TEXT, email TEXT);")
-except psycopg2.errors.DuplicateTable:
-    pass
-
-
-@app.route('/', methods=['POST', 'GET'])
-def info():
-    if request.method == "POST":
-        print(request.form)
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO fight_table VALUES (%s, %s);",
-                    (
-                        request.form.get("name"),
-                        request.form.get("email"),
-                    )
-                )
-    return render_template("index.html")
-
-
-@app.route("/")
-def show_login():
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM fight_table;")
-            fight_table = cursor.fetchall()
-    return render_template("index.html", entries=fight_table)
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -97,6 +62,41 @@ def enable_logging():
     logger.setLevel(logging.DEBUG)
     logging.basicConfig(level=logging.INFO,
                         datefmt=log_datefmt, format=log_format)
+
+
+connection = psycopg2.connect(POSTGRES_URL)
+try:
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "CREATE TABLE fight_table (name TEXT, email TEXT);")
+except psycopg2.errors.DuplicateTable:
+    pass
+
+
+@app.route('/', methods=['POST', 'GET'])
+def info():
+    if request.method == "POST":
+        print(request.form)
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "INSERT INTO fight_table VALUES (%s, %s);",
+                    (
+                        request.form.get("name"),
+                        request.form.get("email"),
+                    )
+                )
+    return render_template("index.html")
+
+
+@app.route("/logins", methods=['POST', 'GET'])
+def show_login():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM fight_table;")
+            fight_table = cursor.fetchall()
+    return render_template("logins.html", entries=fight_table)
 
 
 if __name__ == '__main__':
